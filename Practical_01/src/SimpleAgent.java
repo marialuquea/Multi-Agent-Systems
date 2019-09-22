ca4e341081901b118a368a1a5068726d6a109293
Maria Luque Anguita <mluqueanguita@gmail.com>
2019-09-22 17:53:17 +0100

    starting the workbook

--- Practical_01/src/SimpleAgent.java
+++ Practical_01/src/SimpleAgent.java
@@ -1,4 +1,8 @@
 import jade.core.Agent;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
 
 public class SimpleAgent extends Agent {
 
@@ -6,6 +10,18 @@ public class SimpleAgent extends Agent {
 	protected void setup() {
 		//Print out a welcome message
 		System.out.println("Hello! Agent "+getAID().getName()+" is ready.");
+		
+		DFAgentDescription dfd = new DFAgentDescription () ;
+		dfd . setName ( getAID () ) ;
+		ServiceDescription sd = new ServiceDescription () ;
+		sd . setType ("Simple - agent ") ;
+		sd . setName ( getLocalName () + "-Simple - agent ") ;
+		dfd . addServices ( sd ) ;
+		try{
+			DFService . register (this , dfd ) ;
+		}
+		catch ( FIPAException e ) {
+			e . printStackTrace () ;
+		}
 	}
-
 }
