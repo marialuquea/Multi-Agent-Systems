f1ace521b24173ed5281f6c0af2b18eaf9667502
Maria Luque Anguita <mluqueanguita@gmail.com>
2019-09-22 18:01:30 +0100

    directory facilitator

--- Practical_01/src/SimpleAgent.java
+++ Practical_01/src/SimpleAgent.java
@@ -11,17 +11,30 @@ public class SimpleAgent extends Agent {
 		//Print out a welcome message
 		System.out.println("Hello! Agent "+getAID().getName()+" is ready.");
 		
-		DFAgentDescription dfd = new DFAgentDescription () ;
-		dfd . setName ( getAID () ) ;
-		ServiceDescription sd = new ServiceDescription () ;
-		sd . setType ("Simple - agent ") ;
-		sd . setName ( getLocalName () + "-Simple - agent ") ;
-		dfd . addServices ( sd ) ;
+		// Registering agent with the Directory Facilitator (yellow pages) 
+		DFAgentDescription dfd = new DFAgentDescription();
+		// registering AID along with a description of the service that the agent provides to others (in this case "Simple agent")
+		// good habit: registering all the agents with the DF so that they can be found at runtime by other agents (possibly running on other computers and written by other people)
+		dfd.setName(getAID());
+		ServiceDescription sd = new ServiceDescription();
+		sd.setType("Simple-agent");
+		sd.setName(getLocalName() + "-Simple-agent");
+		dfd.addServices(sd);
 		try{
-			DFService . register (this , dfd ) ;
+			DFService.register(this, dfd);
 		}
-		catch ( FIPAException e ) {
-			e . printStackTrace () ;
+		catch(FIPAException e){
+			e.printStackTrace();
+		}
+	}
+	
+	// de-registering agent with yellow pages when it terminates
+	protected void takeDown() {
+		try {
+			DFService.deregister(this);
+		}
+		catch(FIPAException e) {
+			e.printStackTrace();
 		}
 	}
 }
