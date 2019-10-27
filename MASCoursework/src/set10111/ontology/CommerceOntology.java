9185993bf95eadf5fc188274d907c8058cecb62a
Maria Luque Anguita <mluqueanguita@gmail.com>
2019-10-27 20:58:25 +0000

    started coursework

---
+++ MASCoursework/src/set10111/ontology/CommerceOntology.java
@@ -0,0 +1,23 @@
+package set10111.ontology;
+
+import jade.content.onto.BeanOntology;
+import jade.content.onto.BeanOntologyException;
+import jade.content.onto.Ontology;
+
+public class CommerceOntology extends BeanOntology
+{
+	private static Ontology theInstance = new CommerceOntology("my_ontology");
+
+	public static Ontology getInstance(){
+		return theInstance;
+	}
+	//singleton pattern
+	private CommerceOntology(String name) {
+		super(name);
+		try {
+			add("set10111.music_shop_ontology.elements");
+		} catch (BeanOntologyException e) {
+			e.printStackTrace();
+		}
+	}
+}
