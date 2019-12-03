0f67f9290d03c3070b6ecf25eb82283e35350107
Maria Luque Anguita <mluqueanguita@gmail.com>
2019-12-03 15:23:35 +0000

    clean folder

--- uk_ontology/src/napier/ac/uk_ontology/computerComponents/Motherboard.java
+++
@@ -1,7 +0,0 @@
-package napier.ac.uk_ontology.computerComponents;
-
-import napier.ac.uk_ontology.concepts.ComputerComponent;
-
-public abstract class Motherboard extends ComputerComponent {
-  private static final long serialVersionUID = 1L;
-}
--- uk_ontology/src/napier/ac/uk_ontology/computerComponents/Os.java
+++
@@ -1,7 +0,0 @@
-package napier.ac.uk_ontology.computerComponents;
-
-import napier.ac.uk_ontology.concepts.ComputerComponent;
-
-public abstract class Os extends ComputerComponent {
-  private static final long serialVersionUID = 1L;
-}
